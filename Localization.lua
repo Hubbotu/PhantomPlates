b79d82fa231558676f5e72dbe3684338c4aea5f2
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-05-02 13:49:23 +0300

    Add files via upload

---
+++ Localization.lua
@@ -0,0 +1,142 @@
+﻿--[[	PhantomPlates Localization
+	by SDPhantom
+	https://www.wowinterface.com/forums/member.php?u=34145
+	https://www.curseforge.com/members/sdphantomgamer/projects	]]
+--------------------------------------------------------------------------
+
+----------------------------------
+--[[	Localization Table	]]
+----------------------------------
+local Localization={
+	enUS={--	Native locale by SDPhantom
+--		Options Panel
+		Options_ByAuthor_Format="by %s";
+
+--		Options Panel (Settings)
+		OptionsSetting_PowerBars_Enabled="Enable Power Bars";
+		OptionsSetting_NamePlateText_ShowHealth="Show Health Text";
+		OptionsSetting_NamePlateText_ShowPower="Show Power Text";
+		OptionsSetting_NamePlateText_AbbreviateNumber="Abbreviate Numbers";
+		OptionsSetting_CastBar_Enabled="Enable Cast Bars";
+		OptionsSetting_CastBarText_ShowCastTime="Show Cast Time";
+		OptionsSetting_CastBarText_ShowCastDiff="Show Cast Pushback";
+		OptionsSetting_Classification_ShowRare="Show Rare Textures";
+		OptionsSetting_Classification_ShowElite="Show Elite Textures";
+		OptionsSetting_BuffFrames_ShowBuffs="Show Buffs";
+		OptionsSetting_BuffFrames_ShowCC="Show Crowd Control";
+		OptionsSetting_ThreatGlow_Enabled="Enable Threat Glow";
+		OptionsSetting_PersonalNamePlate_Enabled="Enable Personal NamePlate";
+		OptionsSetting_PersonalNamePlate_AlwaysShow="Always Show";
+		OptionsSetting_PersonalNamePlate_EnableCastBar="Show Personal Cast Bar";
+		OptionsSetting_PersonalNamePlate_ShowTradeskills="Show Tradeskills";
+		OptionsSetting_PersonalNamePlate_HideDefaultCastBar="Disable Original Cast Bar";
+		OptionsSetting_PersonalNamePlate_YPosFormat="Y Offset: %d";
+
+--		Numbers
+		NumberGroupingScale=3;--	Controls scale of FIRST_NUMBER_CAP and SECOND_NUMBER_CAP
+	};
+
+	deDE={--	Translated by val1n
+--		Options Panel
+		Options_ByAuthor_Format="von %s";
+
+--		Options Panel (Settings)
+		OptionsSetting_PowerBars_Enabled="Aktiviere Kraftleiste";
+		OptionsSetting_NamePlateText_ShowHealth="Zeige Lebenstext";
+		OptionsSetting_NamePlateText_ShowPower="Zeige Ressourcentext";
+		OptionsSetting_NamePlateText_AbbreviateNumber="Zahlen abkürzen";
+		OptionsSetting_Classification_ShowRare="Zeige Rare Textur";
+		OptionsSetting_Classification_ShowElite="Zeige Elite Textur";
+		OptionsSetting_BuffFrames_ShowBuffs="Zeige Verstärkungen";
+		OptionsSetting_BuffFrames_ShowCC="Zeige Massenkontrolleffekte";
+		OptionsSetting_ThreatGlow_Enabled="Aktiviere Bedrohungsleuchten";
+		OptionsSetting_PersonalNamePlate_Enabled="Aktiviere eigene Lebensplakette";
+		OptionsSetting_PersonalNamePlate_YPosFormat="Y-Versatz: %d";
+
+--		Numbers
+		NumberGroupingScale=3;--	Controls scale of FIRST_NUMBER_CAP and SECOND_NUMBER_CAP
+	};
+	frFR={--	Translated by Arektor
+--		Options Panel
+		Options_ByAuthor_Format="par %s";
+
+--		Options Panel (Settings)
+		OptionsSetting_PowerBars_Enabled="Activer les barres de ressource";
+		OptionsSetting_NamePlateText_ShowHealth="Afficher les points de vie";
+		OptionsSetting_NamePlateText_ShowPower="Afficher les ressources";
+		OptionsSetting_NamePlateText_AbbreviateNumber="Abréger les nombres";
+		OptionsSetting_Classification_ShowRare="Montrer la texture des rares";
+		OptionsSetting_Classification_ShowElite="Montrer la texture des élites";
+		OptionsSetting_BuffFrames_ShowBuffs="Montrer les améliorations";
+		OptionsSetting_BuffFrames_ShowCC="Montrer les contrôles";
+		OptionsSetting_ThreatGlow_Enabled="Activer la surbrillance de menace";
+		OptionsSetting_PersonalNamePlate_Enabled="Activer la barre personnelle";
+		OptionsSetting_PersonalNamePlate_YPosFormat="Décalage en hauteur : %d";
+
+--		Numbers
+		NumberGroupingScale=3;--	Controls scale of FIRST_NUMBER_CAP and SECOND_NUMBER_CAP
+	};
+	koKR={
+		NumberGroupingScale=4;--	Controls scale of FIRST_NUMBER_CAP and SECOND_NUMBER_CAP
+	};
+	zhCN={--    Translated by 颜颜@莫格莱尼
+--		Options Panel
+		Options_ByAuthor_Format="作者 %s";
+
+--		Options Panel (Settings)
+		OptionsSetting_PowerBars_Enabled="显示法力/怒气/能量条";
+		OptionsSetting_NamePlateText_ShowHealth="显示生命值";
+		OptionsSetting_NamePlateText_ShowPower="显示法力/怒气/能量值";
+		OptionsSetting_NamePlateText_AbbreviateNumber="简短数字格式";
+		OptionsSetting_CastBarText_ShowCastTime="显示施法时间";
+		OptionsSetting_CastBarText_ShowCastDiff="显示施法击退时间";
+		OptionsSetting_Classification_ShowRare="显示稀有边框";
+		OptionsSetting_Classification_ShowElite="显示精英边框";
+		OptionsSetting_BuffFrames_ShowBuffs="显示 Buff";
+		OptionsSetting_BuffFrames_ShowCC="显示昏迷、减速或定身";
+		OptionsSetting_ThreatGlow_Enabled="开启仇恨发光";
+		OptionsSetting_PersonalNamePlate_Enabled="显示个人姓名板";
+		OptionsSetting_PersonalNamePlate_AlwaysShow="总是显示";
+		OptionsSetting_PersonalNamePlate_YPosFormat="Y 偏移: %d";
+
+		NumberGroupingScale=4;--	Controls scale of FIRST_NUMBER_CAP and SECOND_NUMBER_CAP
+	};
+	zhTW={
+		NumberGroupingScale=4;--	Controls scale of FIRST_NUMBER_CAP and SECOND_NUMBER_CAP
+	};
+	ruRU={--	Translated to Russian ZamestoTV
+--		Options Panel
+		Options_ByAuthor_Format="от %s";
+
+--		Options Panel (Settings)
+		OptionsSetting_PowerBars_Enabled="Включить полосы энергии";
+		OptionsSetting_NamePlateText_ShowHealth="Показывать текст здоровья";
+		OptionsSetting_NamePlateText_ShowPower="Показывать текст энергии";
+		OptionsSetting_NamePlateText_AbbreviateNumber="Сокращать числа";
+		OptionsSetting_CastBar_Enabled="Включить полосы заклинаний";
+		OptionsSetting_CastBarText_ShowCastTime="Показывать время произнесения";
+		OptionsSetting_CastBarText_ShowCastDiff="Показывать отбрасывание заклинания";
+		OptionsSetting_Classification_ShowRare="Показывать текстуры редких";
+		OptionsSetting_Classification_ShowElite="Показывать текстуры элитных";
+		OptionsSetting_BuffFrames_ShowBuffs="Показывать баффы";
+		OptionsSetting_BuffFrames_ShowCC="Показывать эффекты контроля";
+		OptionsSetting_ThreatGlow_Enabled="Включить свечение угрозы";
+		OptionsSetting_PersonalNamePlate_Enabled="Включить личную табличку";
+		OptionsSetting_PersonalNamePlate_AlwaysShow="Всегда показывать";
+		OptionsSetting_PersonalNamePlate_EnableCastBar="Показывать личную полосу заклинаний";
+		OptionsSetting_PersonalNamePlate_ShowTradeskills="Показывать ремесла";
+		OptionsSetting_PersonalNamePlate_HideDefaultCastBar="Отключить оригинальную полосу заклинаний";
+		OptionsSetting_PersonalNamePlate_YPosFormat="Смещение по Y: %d";
+
+--		Numbers
+		NumberGroupingScale=3;--	Controls scale of FIRST_NUMBER_CAP and SECOND_NUMBER_CAP
+	};
+}
+
+----------------------------------
+--[[	Localization Loader	]]--	Nothing to localize below this line
+----------------------------------
+local AddOn=select(2,...);
+setmetatable(Localization.enUS,{__index=function(t,k) return k; end});
+AddOn.Localization=Localization[GetLocale()] or Localization.enUS;
+if AddOn.Localization~=Localization.enUS then setmetatable(AddOn.Localization,{__index=Localization.enUS}); end
